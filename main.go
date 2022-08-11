296573aac721b14bfb9b7f8725e4989477f42bab
Ahmetcan Aksu <ahmetcanco@gmail.com>
2022-08-11 14:46:37 +0300

    Fix comma

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func main() {
 	router.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"*"},
-		AllowHeaders:     []string{"*",
+		AllowHeaders:     []string{"*"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		AllowOriginFunc: func(origin string) bool {
